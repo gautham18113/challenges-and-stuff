5d536878f17b94c73bdf80ee441c3c2333749133
gautham18113 <gautham18113@gmail.com>
2022-06-25 14:28:17 -0700

    add optimized solution for reconstruct BST

--- src/main/java/solutions/graphs/binarytree/ReconstructBSTSolution.java
+++ src/main/java/solutions/graphs/binarytree/ReconstructBSTSolution.java
@@ -10,7 +10,8 @@ public class ReconstructBSTSolution implements Solution{
     @Override
     public Output<?> solve(Input<?> input) {
         int[] array = (int[]) input.getInput();
-        BST result = reconstructBst(array, 0, array.length-1);
+        reconstructBst(array, 0, array.length-1);
+        BST result = reconstructBstOptimized(array, Integer.MIN_VALUE, Integer.MAX_VALUE, new GlobalVariables(), null);
         return new Output<>(result);
     }
 
@@ -62,4 +63,43 @@ public class ReconstructBSTSolution implements Solution{
 
     }
 
-}
+    class GlobalVariables {
+        public int currentIdx;
+    }
+
+    /**
+     * <p>
+     * In this solution, we use the concept of minimum and maximum possible values allowable for a BST
+     * node. This concept previously used in {@link ValidateBSTSolution}. We use minimum and maximum
+     * allowable values to check if value to be inserted belongs at a certain position. We keep track
+     * of value to be inserted globally in the instance object of {@link GlobalVariables}.
+     * </p>
+     * <p>
+     * <b>T</b> O(n) <b>S</b> O(n + h)
+     * </p>
+     * @param array
+     * @param minPossibleVal
+     * @param maxPossibleVal
+     * @param v
+     * @param tree
+     * @return
+     */
+    private final BST reconstructBstOptimized(int[] array, int minPossibleVal, int maxPossibleVal, GlobalVariables v, BST tree) {
+
+        if(v.currentIdx > array.length - 1) return null;
+
+        int currentVal = array[v.currentIdx];
+
+        if(currentVal >= minPossibleVal && currentVal < maxPossibleVal) {
+            tree = new BST(currentVal);
+        } else {
+            return null;
+        }
+
+        v.currentIdx++;
+
+        tree.left = reconstructBstOptimized(array, minPossibleVal, currentVal, v, tree.left);
+        tree.right = reconstructBstOptimized(array , currentVal, maxPossibleVal, v, tree.right);
+
+        return tree;
+    } }
