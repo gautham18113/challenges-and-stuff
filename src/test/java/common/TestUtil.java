5aa2d37f8a5cf600fa140aa70875887481b7ad84
gautham18113 <gautham18113@gmail.com>
2023-04-05 23:46:42 -0700

    Add condition to handle list comparison

--- src/test/java/common/TestUtil.java
+++ src/test/java/common/TestUtil.java
@@ -5,40 +5,55 @@ import core.Solution;
 import core.TestCase;
 import core.datastructure.BST;
 import core.io.Input;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 
 public class TestUtil {
 
     @SuppressWarnings("all")
-    public static void runAssertions(
-        ProblemInterface problem,
-        Solution solution
-    ) {
-        for(TestCase test: problem.getProblem().getTestCases()) {
+    public static void runAssertions(ProblemInterface problem, Solution solution) {
+        for (TestCase test : problem.getProblem().getTestCases()) {
             Input input = test.getInputs().get(0);
 
             Object actual = solution.solve(input).getOutput();
             Object expected = test.getOutput().getOutput();
 
-            if(actual instanceof BST && expected instanceof BST) {
+            if (actual instanceof BST && expected instanceof BST) {
                 Assertions.assertThat(actual.equals(expected));
-            }
-            else {
-                Assertions
-                    .assertThat(actual)
-                    .isEqualTo(expected);
+            } else if (actual instanceof List && expected instanceof  List) {
+                assert(((List<?>) actual).containsAll((List<?>) expected));
+            } else {
+                Assertions.assertThat(actual).isEqualTo(expected);
             }
         }
     }
 
-    public static void runAssertionsMultipleParams(
-        ProblemInterface problem,
-        Solution solution
-    ) {
-        for(TestCase test: problem.getProblem().getTestCases()) {
-            Assertions
-                .assertThat(solution.solve(new Input<>(test.getInputs())).getOutput())
-                .isEqualTo(test.getOutput().getOutput());
+    @SuppressWarnings("unchecked")
+    public static void runAssertionsWithComparator(ProblemInterface problem, Solution solution, Comparator comparator) {
+        for (TestCase test : problem.getProblem().getTestCases()) {
+            Input input = test.getInputs().get(0);
+
+            Object actual = solution.solve(input).getOutput();
+            Object expected = test.getOutput().getOutput();
+
+            if (actual instanceof List) {
+                Collections.sort((List) actual, new ListComparator<>());
+                Collections.sort((List) expected, new ListComparator<>());
+            }
+
+            Assertions.assertThat(actual).isEqualTo(expected);
+
+        }
+    }
+
+    public static void runAssertionsMultipleParams(ProblemInterface problem, Solution solution) {
+        for (TestCase test : problem.getProblem().getTestCases()) {
+            Assertions.assertThat(solution.solve(new Input<>(test.getInputs())).getOutput())
+                    .isEqualTo(test.getOutput().getOutput());
         }
     }
 }
